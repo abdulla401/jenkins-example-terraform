59f7d59a1395e5562025f01dd459d5847d2eee85
Abdulla Syed <asyed@mimecast.com>
2022-09-13 20:54:45 +0100

    terraform s3 changes

--- main.tf
+++ main.tf
@@ -1,3 +1,11 @@
+provider "aws" {
+  region = "eu-west-1"
+}
+
+resource "aws_s3_bucket" "first_bucket" {
+  bucket = "abd401-first-bucket"
+}
+
 output "jenkins_terraform" {
   value = "running Terraform from Jenkins"
 }
